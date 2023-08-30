75136c20b06cf19ed771b0865891caabc8958025
latifahassan <latifahassanali@outlook.com>
2023-08-30 23:00:50 +0100

    changes made to verlay

--- ThePerfectTrip/src/components/video.jsx
+++ ThePerfectTrip/src/components/video.jsx
@@ -1,48 +1,23 @@
-import { useState, useEffect } from 'react';
 import './video.css';
 import backdrop from '../assets/perfect.mp4';
 import perfectLogo from './the-perfect-trip-logo.png'; // Make sure the image path is correct
+//import { Link } from 'react-router-dom'
+
 
 const Video = () => {
-  const texts = [
-    'Cannot decide on a holiday destination?',
-    'Let us help you!',
-    'Have fun!',
-  ];
-
-  const [currentTextIndex, setCurrentTextIndex] = useState(0);
-
-  useEffect(() => {
-    const delay = 7000; // milliseconds
-
-    const interval = setInterval(() => {
-      setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
-    }, delay);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div className='main'>
-      <video className="video-element" src={backdrop} autoPlay loop muted />
-      <div className="overlay">
-        <div className='content'>
-          <h1 className="perfect-trip-sign">The Perfect Trip</h1>
-          <img src={perfectLogo} alt="The Perfect Trip Logo" className="logo" />
-          {texts.map((text, index) => (
-            <h1
-              key={index}
-              className={`fade-in-text ${currentTextIndex === index ? 'show' : ''}`}
-            >
-              {text}
-            </h1>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+return (
+<div className='main'>
+<video className="video-element" src={backdrop} autoPlay loop muted />
+<div className="overlay">
+<div className='content'>
+<img src={perfectLogo} alt="The Perfect Trip Logo" className="logo" />
+<h1 className="static-text-top">An Amazing trip Starts with You</h1>
+<h1 className="static-text plan-trip">Plan the trip of a Lifetime</h1>
+</div>
+</div>
+</div>
+);
 };
+
 
 export default Video;
